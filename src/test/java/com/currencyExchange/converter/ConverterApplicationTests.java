85a8a156f0fd1f8d4f346ce06cd50e2a6fc7ae12
Aman Bhojraj Shende <aman.shende@accolite.com>
2023-02-23 09:34:49 +0530

    init

---
+++ src/test/java/com/currencyExchange/converter/ConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currencyExchange.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
